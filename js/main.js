883f984199418673746206b62c2eb345dc389734
MikiyaYamada <m.photo7217@gmail.com>
2025-05-26 16:19:30 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -26,3 +26,12 @@ document.addEventListener("DOMContentLoaded", () => {
     observer.observe(el);
   });
 });
+
+function toggleMenu(btn) {
+  // ハンバーガー自体に .active クラスをトグル
+  btn.classList.toggle("active");
+
+  // メニュー本体を開閉
+  const nav = document.getElementById("nav-menu");
+  nav.classList.toggle("active");
+}
